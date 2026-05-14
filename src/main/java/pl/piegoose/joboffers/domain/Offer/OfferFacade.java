a13518704f8a65a849287b3ad702395041899628
piegoose <jedrzej.sikora@puszkin.eu>
2026-05-14 11:34:57 +0200

    created classes LoginAndRegisterFacade, OfferFacade.java, JwtAuthenticator, refactor

---
+++ src/main/java/pl/piegoose/joboffers/domain/Offer/OfferFacade.java
@@ -0,0 +1,8 @@
+package pl.piegoose.joboffers.domain.Offer;
+
+public class OfferFacade {
+    public void findAllOffers(){}
+    public void findOfferById(){}
+    public void saveOffer(){}
+    public void fetchAllOffersAndSaveAllIfNotExists(){}
+}
