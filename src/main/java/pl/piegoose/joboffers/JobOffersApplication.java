53d06a1dbb0787e5a8351ab52f47b8bf6b5cce46
piegoose <jedrzej.sikora@puszkin.eu>
2026-05-14 11:08:34 +0200

    Project created

---
+++ src/main/java/pl/piegoose/joboffers/JobOffersApplication.java
@@ -0,0 +1,13 @@
+package pl.piegoose.joboffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOffersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobOffersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/piegoose/joboffers/JobOffersApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.piegoose.joboffers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOffersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
